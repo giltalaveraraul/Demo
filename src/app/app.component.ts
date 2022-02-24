3008ead2cf618c887a7e9863a945a8e68a258657
Raúl Gil <rgil@vectoritcgroup.com>
2022-02-24 02:14:19 +0100

    Feature/docker advanced

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,44 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Config, ConfigService } from './config/config.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [ ConfigService ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  error: any;
   who = {} as any;
+  config: Config |undefined;
 
   
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private configService: ConfigService) {}
 
   whoAreYou(){
-    this.http.get(environment.url).subscribe(data => {
-      this.who =data;
-    },
-    error => {
-      console.log(error);
-      this.who = {"name":"world"};
-    })
+
+    this.configService.getConfig()
+    .subscribe({
+      next: (data: Config) => {
+        this.config = { ...data };
+        this.http.get(this.config.whoUrl).subscribe(data => {
+          this.who =data;
+        },
+        error => {
+          console.log(error);
+          this.who = {"name":"world"};
+        })
+      }, // error path // success path
+      error: error => {
+        this.error = error;
+        console.log(error);
+        this.who = {"name":"world"};
+      }
+    });
+    
   }
 }
